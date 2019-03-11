02f7a980a4c677433f4c58e458da147413caa7fe
Ajah Dailey <ajahdailey@zipcoders-MacBook-Pro-5.local>
2019-03-11 17:25:54 -0400

    classes, no test

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,18 @@
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+    public Integer toss(){
+        Random rand = new Random();
+
+
+        int num = rand.nextInt(6);
+
+
+        num += 1;
+
+        return  num;
+
+
+    }
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,24 @@
 public class Simulation {
+    Bins bins = new Bins();
+    private Dice dice = new Dice();
 
+    public Bins getBins() {
+        return bins;
+    }
+
+    public void rollingTheDice(){
+        for (int i = 0; i < 20; i++) {
+            bins.store(dice.toss());
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation();
+        simulation.rollingTheDice();
+        System.out.println(simulation.getBins().bins.toString());
+    }
 
 
 }
